51d9c93ca0a6aa7b5ffd5ec3a1fa3f5f98458feb
AlexanderGorgonzola <alex.garakanidze@protonmail.com>
2024-07-30 06:51:22 -0700

    logging is confusing and i should come back to it later

--- main_loggin.py
+++ main_loggin.py
@@ -1,4 +1,9 @@
 import logging
+import logging.config
+
+#logging.config.fileConfig("logging.conf") <-- specific file configurations
+
+#Check the documentation for more info
 
 #logging.basicConfig() <-- basic configurations
 logger = logging.getLogger(__name__)
@@ -18,3 +23,9 @@ logger.addHandler(file_h)
 
 logger.warning("This is a warning")
 logger.error("This is an error")
+
+try:
+    a = [1,2,3]
+    val = a[4]
+except IndexError as e:
+    logging.error(e, exc_info=True)
