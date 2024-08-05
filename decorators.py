60a1b6a7ddebb167695f7a8009f6c1d202209f6d
AlexanderGorgonzola <alex.garakanidze@protonmail.com>
2024-08-05 07:04:08 -0700

    Thats all folks! (looney toons outro starts playing)

--- decorators.py
+++ decorators.py
@@ -26,3 +26,24 @@ print(result)
 
 print(help(add5))
 print(add5.__name__)
+
+
+
+def repeat(num_times):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator_repeat
+
+@repeat(num_times=4)
+def greet(name):
+    print(f"Hi fat {name}")
+
+greet("wivchj") #erm what the sigma
+
+#Ill find out more about this later
+"""I'll be using these every once in a while""" 
